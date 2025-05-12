9e7129aea537b465055d86f253b3bf6dea3e31e6
Hanssbr <Hanssbr@users.noreply.github.com>
2025-05-12 06:38:01 +0000

    deploy: 3afd700da067176167b1349d89a0e23b464a8f9d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1370672841"
+    serviceWorkerVersion: "1285068292"
   }
 });
